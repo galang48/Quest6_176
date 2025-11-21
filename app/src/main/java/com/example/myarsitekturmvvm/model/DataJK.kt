5ec871603a0bb2213ae6566667490b6f0de3eb05
galang48 <galangyudha1945@gmail.com>
2025-11-21 10:11:10 +0700

    *Menambahkan model `DataJK` berisi daftar `JenisK` (Laki-laki, Perempuan).*

---
+++ app/src/main/java/com/example/myarsitekturmvvm/model/DataJK.kt
@@ -0,0 +1,10 @@
+package com.example.myarsitekturmvvm.model
+
+import com.example.myarsitekturmvvm.R
+
+object DataJK {
+    val JenisK = listOf(
+        "Laki-laki",
+        "Perempuan"
+    )
+}
